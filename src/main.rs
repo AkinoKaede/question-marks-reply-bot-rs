ed55b4d87e5586e03262ace159a548345c95a4a5
AkinoKaede <i@akinokae.de>
2023-12-01 10:41:33 +0800

    feat: add webhook

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use teloxide::{prelude::*, RequestError};
+use teloxide::{prelude::*, RequestError, update_listeners::webhooks};
 use teloxide::types::{MediaKind, MediaSticker, MediaText, MessageCommon, MessageKind};
 
 mod question_mark_reply;
@@ -11,6 +11,12 @@ mod question_mark_reply;
 struct Cli {
     #[arg(short, long)]
     token: Option<String>,
+
+    #[arg(short, long)]
+    address: Option<String>,
+
+    #[arg(short, long)]
+    url: Option<String>,
 }
 
 #[tokio::main]
@@ -25,11 +31,34 @@ async fn main() {
         None => Bot::from_env(),
     };
 
+    let listener = match cli.address {
+        Some(address) => {
+            let address = address.parse().expect("Unable to parse socket address.");
+            let url = cli.url.expect("The Url must be set.");
+            let url = url.parse().unwrap();
 
-    teloxide::repl(bot, handler).await;
+            let axum = webhooks::axum(bot.clone(), webhooks::Options::new(address, url))
+                .await
+                .expect("Couldn't setup webhook");
+
+            Some(axum)
+        }
+
+        _ => None,
+    };
+
+
+    match listener {
+        Some(listener) => {
+            teloxide::repl_with_listener(bot, message_handler, listener).await
+        }
+        None => {
+            teloxide::repl(bot, message_handler).await
+        }
+    }
 }
 
-async fn handler(bot: Bot, msg: Message) -> Result<(), RequestError> {
+async fn message_handler(bot: Bot, msg: Message) -> Result<(), RequestError> {
     match msg.kind {
         MessageKind::Common(
             MessageCommon {
