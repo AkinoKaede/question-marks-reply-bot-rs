a083ec246856732f6b33015630bb10f286e19ddd
AkinoKaede <i@akinokae.de>
2023-12-01 09:46:41 +0800

    feat: add token arg

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
+use clap::Parser;
 use teloxide::{prelude::*, RequestError};
 use teloxide::types::{MediaKind, MediaSticker, MediaText, MessageCommon, MessageKind};
 
 mod question_mark_reply;
 
+#[derive(Parser)]
+#[command(name = "question-marks-reply-bot-rs")]
+#[command(version = "0.1.0")]
+#[command(about = "A Telegram bot for reply question marks.", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    token: Option<String>,
+}
+
 #[tokio::main]
 async fn main() {
+    let cli = Cli::parse();
+
     pretty_env_logger::init();
     log::info!("Starting question marks reply bot...");
-    let bot = Bot::from_env();
+
+    let bot = match cli.token {
+        Some(token) => Bot::new(token),
+        None => Bot::from_env(),
+    };
 
 
     teloxide::repl(bot, handler).await;
