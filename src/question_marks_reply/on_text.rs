0771ee85b2c4277c7b410ac2cf2c8c1b75c3ff26
Kaede Akino <i@akinokae.de>
2024-02-08 10:41:19 +0800

    fix: reply not composed of question marks

--- src/question_marks_reply/on_text.rs
+++ src/question_marks_reply/on_text.rs
@@ -6,27 +6,27 @@ use crate::question_marks_reply::{IsComposedOfSameChar, QuestionMarks};
 pub(crate) async fn on_text(bot: Bot, msg: Message) -> Result<(), RequestError> {
     if let Some(text) = msg.text() {
         let text = text.to_string();
+        if text.is_composed_of_question_marks() {
 
-        if let Some(reply_msg) = msg.reply_to_message() {
-            if let Some(reply_text) = reply_msg.text() {
-                let reply_text = reply_text.to_string();
+            if let Some(reply_msg) = msg.reply_to_message() {
+                if let Some(reply_text) = reply_msg.text() {
+                    let reply_text = reply_text.to_string();
 
-                if text.is_composed_of_same_char().unwrap_or(false)
-                    && reply_text.is_composed_of_same_char().unwrap_or(false) {
-                    let chr = text.chars().next().unwrap();
+                    if text.is_composed_of_same_char().unwrap_or(false)
+                        && reply_text.is_composed_of_same_char().unwrap_or(false) {
+                        let chr = text.chars().next().unwrap();
 
-                    if text.chars().filter(|c| *c == chr).count()
-                        == reply_text.chars().filter(|c| *c == chr).count() + 1 {
-                        bot.send_message(msg.chat.id, format!("{}{}", text, chr))
-                            .reply_to_message_id(msg.id).await?;
+                        if text.chars().filter(|c| *c == chr).count()
+                            == reply_text.chars().filter(|c| *c == chr).count() + 1 {
+                            bot.send_message(msg.chat.id, format!("{}{}", text, chr))
+                                .reply_to_message_id(msg.id).await?;
 
-                        return Ok(());
+                            return Ok(());
+                        }
                     }
                 }
             }
-        }
 
-        if text.is_composed_of_question_marks() {
             if let Some(rev) = text.rev() {
                 bot.send_message(msg.chat.id, rev).reply_to_message_id(msg.id).await?;
                 return Ok(());
